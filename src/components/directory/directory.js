50b66ff6816c9b907da066ba9840428b5d27fc88
AidaMahyazadeh <aidamahyazadeh83@gmail.com>
2023-04-04 09:34:36 +0200

    first commit

---
+++ src/components/directory/directory.js
@@ -0,0 +1,13 @@
+import CategoryItem from "../category-item/category-item";
+import './directory.styles.scss';
+
+function Directory (props){
+    return(
+        <div className='directory-container'>
+        {props.categories.map((category)=>(
+        <CategoryItem key={category.id} category={category}/>
+        ))}
+      </div>
+    )
+}
+export default Directory;
